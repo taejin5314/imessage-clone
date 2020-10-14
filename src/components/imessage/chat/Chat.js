8f069d7c8a2bb8dd432214d160710c9bb1e21d85
taejin <taejin5314@gmail.com>
2020-10-14 19:48:03 -0400

    Component: Chat

--- src/components/imessage/chat/Chat.js
+++ src/components/imessage/chat/Chat.js
@@ -4,7 +4,13 @@ import './Chat.css'
 function Chat() {
     return (
         <div className="chat">
-
+            {/* ChatHeader */}
+            <div className="chat__header">
+                <h4>To: <span className="chat__name">Channel Name</span></h4>
+                <strong>Details</strong>
+            </div>
+            {/* ChatMessages */}
+            {/* ChatInput */}
         </div>
     )
 }
