9dc0549f52849fc0e9d14c5a0d4ebf129bf110ef
taejin <taejin5314@gmail.com>
2020-10-15 12:52:37 -0400

    Component: Chat

--- src/components/imessage/chat/Chat.js
+++ src/components/imessage/chat/Chat.js
@@ -14,11 +14,19 @@ function Chat() {
                 <h4>To: <span className="chat__name">Channel Name</span></h4>
                 <strong>Details</strong>
             </div>
+
             {/* ChatMessages */}
+
             {/* ChatInput */}
             <div className="chat__input">
-                <input placeholder="iMessage" value={Input} onChange={(e) => setInput(e.target.value)} type="text" />
-                <button onClick={sendMessage}>Send Message</button>
+                <form>
+                    <input
+                        placeholder="iMessage"
+                        value={Input}
+                        onChange={(e) => setInput(e.target.value)} type="text"
+                    />
+                    <button onClick={sendMessage}>Send Message</button>
+                </form>
             </div>
         </div>
     )
