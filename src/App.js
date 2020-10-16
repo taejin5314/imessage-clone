17c16456583b3db95c6c55642539e0b6f2ec46b6
taejin <taejin5314@gmail.com>
2020-10-16 00:00:13 -0400

    Update Login.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Imessage from './components/imessage/Imessage';
+import Login from './components/login/Login';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Imessage />
+      {user ? <Imessage /> : <Login />}
     </div>
   );
 }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,16 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import './Login.css';
 
 function Login() {
     return (
         <div className="login">
+            <div className="login__logo">
+                <img src='https://upload.wikimedia.org/wikipedia/commons/d/d6/Messages_%28macOS%29.svg' alt="iMessage-logo" />
+                <h1>iMessage</h1>
+            </div>
 
+            <Button>Sign In</Button>
         </div>
     )
 }
