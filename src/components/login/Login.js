122b512bd6fc368e35f24bb7f17ef09b2ca39977
taejin <taejin5314@gmail.com>
2020-10-16 00:02:32 -0400

    Login.js: Sign up with popup function by using firebase.auth

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,14 @@
 import { Button } from '@material-ui/core';
+import { auth, provider } from '../../firebase';
 import React from 'react';
 import './Login.css';
 
 function Login() {
+    const signIn = () => {
+        auth
+            .signInWithPopup(provider)
+            .catch((error) => alert(error.message));
+    }
     return (
         <div className="login">
             <div className="login__logo">
@@ -10,7 +16,7 @@ function Login() {
                 <h1>iMessage</h1>
             </div>
 
-            <Button>Sign In</Button>
+            <Button onClick={signIn} >Sign In</Button>
         </div>
     )
 }
