a3c0f182ce2794824275532a46f8ed9017ba6d3c
taejin <taejin5314@gmail.com>
2020-10-16 19:15:33 -0400

    Update SidebarChat.js

--- src/components/imessage/sidebar/sidebarChat/SidebarChat.js
+++ src/components/imessage/sidebar/sidebarChat/SidebarChat.js
@@ -1,11 +1,23 @@
 import { Avatar } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setChat } from '../../../../features/chatSlice'
+import db from '../../../../firebase';
 import './SidebarChat.css';
 
 function SidebarChat({ id, chatName }) {
     const dispatch = useDispatch();
+    const [ChatInfo, setChatInfo] = useState([]);
+
+    useEffect(() => {
+        db.collection('chats')
+            .doc(id)
+            .collection('messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => (
+                setChatInfo(snapshot.docs.map(doc => doc.data()))
+            ))
+    }, [id])
 
     return (
         <div
@@ -18,11 +30,11 @@ function SidebarChat({ id, chatName }) {
             }}
             className="sidebarChat"
         >
-            <Avatar />
+            <Avatar src={ChatInfo[0]?.photo} />
             <div className="sidebarChat__info">
                 <h3>{chatName}</h3>
-                <p>Last message sent...</p>
-                <small>tiemstamp</small>
+                <p>{ChatInfo[0]?.message}</p>
+                <small>{new Date(ChatInfo[0]?.timestamp.toDate()).toLocaleDateString()}</small>
             </div>
         </div>
     )
